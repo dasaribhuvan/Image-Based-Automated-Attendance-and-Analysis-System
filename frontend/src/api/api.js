047818e487159a3b6a0da4e557c6b78cba431df2
dasariBhuvan <dasaribhuvan123@gmail.com>
2026-04-03 18:22:29 +0530

    Updated api url to railway backend

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
   import axios from "axios"
 
   const API = axios.create({
-    baseURL: "http://127.0.0.1:8000"
+    baseURL: "https://image-based-automated-and-analysis-system-production.up.railway.app"
   })
 
   API.interceptors.request.use((config) => {
