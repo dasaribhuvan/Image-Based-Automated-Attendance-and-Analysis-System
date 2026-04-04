360ecb441b79d6d9d3b130a992dc8ad34b6d7a0b
dasariBhuvan <dasaribhuvan123@gmail.com>
2026-04-04 20:53:31 +0530

    Latest Commit

--- frontend/src/pages/StudentRegister.jsx
+++ frontend/src/pages/StudentRegister.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import Webcam from "react-webcam";
 import { motion } from "framer-motion";
 import { useNavigate } from "react-router-dom";
